7c11bff626d1a0826f93d38d67ddfd42ba94696b
emi-dev28 <emilianocellilli00@gmail.com>
2023-06-01 22:19:06 -0300

    primer commit, con la config del Spring Boot

---
+++ PI_Emi_Tania/PI_Emi_Tania/src/main/java/com/example/PI_Emi_Tania/PiEmiTaniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PI_Emi_Tania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiEmiTaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiEmiTaniaApplication.class, args);
+	}
+
+}
---
+++ PI_Emi_Tania/PI_Emi_Tania/src/test/java/com/example/PI_Emi_Tania/PiEmiTaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PI_Emi_Tania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiEmiTaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
