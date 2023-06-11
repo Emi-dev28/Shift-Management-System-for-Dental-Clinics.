878e0829ef7bb2fd46a90cf0bcb9f3ac2298948a
emi-dev28 <emilianocellilli00@gmail.com>
2023-06-11 15:52:28 -0300

    correcion PI

--- PI_Emi_Tania/PI_Emi_Tania/src/main/java/com/example/PI_Emi_Tania/PiEmiTaniaApplication.java
+++ PI_Emi_Tania/PI_Emi_Tania/src/main/java/com/example/PI_Emi_Tania/PiEmiTaniaApplication.java
@@ -1,13 +1,21 @@
 package com.example.PI_Emi_Tania;
 
+import com.example.PI_Emi_Tania.Repository.H2Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class PiEmiTaniaApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PiEmiTaniaApplication.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        H2Connection.getConnection();
         SpringApplication.run(PiEmiTaniaApplication.class, args);
+        LOGGER.info("Proyecto Integrador Emi y Tania is running...");
     }
 
 }
